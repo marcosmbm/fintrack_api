9348f3a13c8eac05283a476e5eb2072382b6e874
Marcos Barbosa <marmbmti@gmail.com>
2025-11-11 21:55:33 -0300

    feat: add helpers http response

---
+++ src/controllers/helpers.ts
@@ -0,0 +1,21 @@
+export function makeHttpResponse<T = unknown>(statusCode: number, body: T) {
+  return { statusCode, body };
+}
+
+export function badRequest(body: unknown) {
+  return makeHttpResponse(400, body);
+}
+
+export function internalServerError(body: unknown) {
+  return makeHttpResponse(500, body);
+}
+
+export function errorHandler(error: unknown) {
+  if (error instanceof Error) return badRequest({ message: error.message });
+
+  return internalServerError({ message: "Internal server error." });
+}
+
+export function created<T = unknown>(body: T) {
+  return makeHttpResponse(201, body);
+}
